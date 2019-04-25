43193b945b61ae379dc58363caebc650d41fc607
pstefa1707 <stefap@smc.sa.edu.au>
2019-04-25 11:38:19 +0930

    Basic HTTP webserver, will be used for host.

---
+++ HTTP Host/src/main.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string>
+#include <string.h>
+
+using namespace std; 
+
+int main(int argc, char *argv[]) {
+	struct sockaddr_in address;
+	const int PORT = 25565;
+	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	int addrlen = sizeof(address);
+	if (server_fd < 0)  
+	{
+		perror("cannot create socket"); 
+		return 0; 
+	}
+	memset((char *)&address, 0, sizeof(address));
+	address.sin_port = htons(PORT);
+	address.sin_addr.s_addr = htons(INADDR_ANY);
+	address.sin_family = AF_INET;
+	int bnd = bind(server_fd,(struct sockaddr *)&address, addrlen);
+	if (bnd < 0) 
+	{ 
+		perror("bind failed"); 
+		return 0; 
+	}
+	int ltn = listen(server_fd, 3);
+	if (listen(server_fd, 3) < 0) 
+	{ 
+		perror("In listen"); 
+		exit(EXIT_FAILURE); 
+	}
+	while(1) {
+		printf("\n+++++++ Waiting for new connection ++++++++\n\n");
+		int new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+		if (new_socket<0)
+		{
+			perror("In accept");            
+			exit(EXIT_FAILURE);        
+		}
+		char buffer[1024] = {0};
+		int valread = read( new_socket , buffer, 1024); 
+		printf("%s\n",buffer );
+		if(valread < 0)
+		{ 
+			printf("No bytes are there to read");
+		}
+		string payload = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 8 \n\n Alo-ha!";
+		write(new_socket, payload.c_str() , strlen(payload.c_str()));
+		printf("Message sent: %s\n", payload.c_str());
+		close(new_socket);
+	}
+}
+
